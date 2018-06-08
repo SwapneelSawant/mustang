dc0cdc43022af1493f3deea72745f27593139aea
swaps <sawant.swapneel@gmail.com>
2018-06-09 02:17:18 +0530

    precondition and optional

---
+++ GuavaExamples/src/main/java/com/mustang/rnd/PreconditionExamples.java
@@ -0,0 +1,14 @@
+package com.mustang.rnd;
+
+import com.google.common.base.Preconditions;
+
+public class PreconditionExamples {
+
+    public static void main(String[] args) {
+        PreconditionExamples preconditionExamples = new PreconditionExamples ();
+        Preconditions.checkArgument ( 10 == 11, "is %s too", "not equal" );
+        Preconditions.checkElementIndex ( 10, 3 );
+        Preconditions.checkNotNull ( preconditionExamples );
+        Preconditions.checkState ( true );
+    }
+}
